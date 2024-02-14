706eb9e2fab6b009aa3eb7085ccb94362314d2a6
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-02-13 23:29:55 -0500

    config tests in project

---
+++ tests/index.test.ts
@@ -0,0 +1,12 @@
+describe("App", () => {
+	it("should be true", () => {
+		expect(true).toBe(true);
+	});
+	test("should be 30", () => {
+		const n1 = 10;
+		const n2 = 20;
+
+		const result = n1 + n2;
+		expect(result).toBe(30);
+	});
+});
