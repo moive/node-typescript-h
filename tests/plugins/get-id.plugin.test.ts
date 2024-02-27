c3cdc689192d9b4c2b3f84ef42fda2ea802ec682
Moises Velasquez <mvelasquezdeveloper@gmail.com>
2024-02-26 22:45:12 -0500

    getUUID Adapter

---
+++ tests/plugins/get-id.plugin.test.ts
@@ -0,0 +1,9 @@
+import { getUUID } from "../../src/plugins/get-id.plugin";
+describe("plugins/get-id.plugins.ts", () => {
+	test("getUUID() should return a UUID", () => {
+		const uuid = getUUID();
+
+		expect(typeof uuid).toBe("string");
+		expect(uuid.length).toBe(36);
+	});
+});
